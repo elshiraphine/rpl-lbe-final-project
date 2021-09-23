9e8110a8b64306e576da80f0f91ed81011164481
nfpurnama <akhnauf2@gmail.com>
2021-09-23 10:23:35 +0700

    Linkable Cards
    Made cards as clickable anchors

--- Pages/landing-page/src/component/Card.js
+++ Pages/landing-page/src/component/Card.js
@@ -1,20 +1,17 @@
 import React from 'react'
-import bookicon from "../assets/bookIcon.png"
-import bellicon from "../assets/bellIcon.png"
-import questionicon from "../assets/questionMark.png"
 import "./Card.css"
 
 const Card = (props) => {
 
     return (
-        <div className="container">
+        <a href="#" className="container">
             <h1>{props.head}</h1>
             <div className="image-container">
                 <img src={props.img} alt={props.alt} />
             </div>
             <p>{props.text}</p>
             <div className="shadow-prop"></div>
-        </div>
+        </a>
     )
 }
 
