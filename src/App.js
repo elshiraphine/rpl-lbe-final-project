71a76453323f9c09b95df2294518225a9982436d
iamkiyotaka <erviana.205025@mhs.its.ac.id>
2021-09-26 16:32:20 +0700

    refactor: delete unused var

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Route , Link} from "react-router-dom";
+import {Route} from "react-router-dom";
 import Home from './Home';
 import Alert from './Alert';
 import Learn from './Learn';
